6d051aaca84f4ddab96f4c9f43c63089e59a5d4d
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-15 18:36:05 -0500

    Estilos del formulario completados

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,21 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Campo = styled.div`
-  display: flex;
-  margin-bottom: 1rem;
-  align-items: center;
-`;
-
-const Label = styled.label`
-  flex: 0 0 100px;
-`;
-
 const Formulario = () => {
   return (
     <form>
       <Campo>
         <Label>Marca</Label>
-        <select name="" id="">
+        <Select>
           <option value="">--Selecione--</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
           <option value="asiatico">Americano</option>
-        </select>
+        </Select>
       </Campo>
       <Campo>
         <Label>Año</Label>
-        <select name="" id="">
+        <Select>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -37,16 +27,57 @@ const Formulario = () => {
           <option value="2014">2014</option>
           <option value="2013">2013</option>
           <option value="2012">2012</option>
-        </select>
+        </Select>
       </Campo>
       <Campo>
         <Label>Plan</Label>
-        <input type="radio" name="plan" value="basico" /> Básico
-        <input type="radio" name="plan" value="completo" /> Completo
+        <InputRadio type="radio" name="plan" value="basico" /> Básico
+        <InputRadio type="radio" name="plan" value="completo" /> Completo
       </Campo>
-      <button type="button">Cotizar</button>
+      <Boton type="button">Cotizar</Boton>
     </form>
   );
 };
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  cursor: pointer;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 3s ease;
+  margin-top: 2rem;
+
+  &:hover {
+    background-color: #26c6da;
+    cursor: pointer;
+  }
+`;
 
 export default Formulario;
